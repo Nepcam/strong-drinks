013f752e8d3b27cb419bf41c2f279d067dcc1c54
Nepcam <cameron@aux.network>
2019-04-06 20:02:02 +1300

    header component inn

---
+++ client/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header =() => {
+    return (
+        <h1>
+            <span className='fa fa-beer' />
+            {' '}Strong Drinks{' '}
+            <span className='fa fa-beer' />
+        </h1>
+    )
+}
+
+export default Header
